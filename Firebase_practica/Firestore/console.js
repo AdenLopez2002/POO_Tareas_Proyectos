895f6adf1658bba568d7e525f5806e5300637af3
AdenLopez2002 <79984485+AdenLopez2002@users.noreply.github.com>
2024-04-10 21:22:11 -0600

    Firebase terminada
    App Firebase (lista de compras) presentado en clase.

--- Firebase_practica/Firestore/console.js
+++ Firebase_practica/Firestore/console.js
@@ -16,3 +16,15 @@ addButton.addEventListener("click", function guardarProductoComprado(producto) {
     // Supongamos que 'productosComprados' es la colección donde guardaremos los productos
     firebase.firestore().collection('productosComprados').add(producto);
   });
+
+
+  /*//gasto
+                var sum;
+                lista_carrito.forEach((item)=> {
+                    var subsum = (item.cantidad.value)*(item.precio.value)
+                    console.log(subsum)
+                    sum += subsum.value
+                });
+                const sumaContainer = document.getElementById("Resultado")
+                sumaContainer.innerHTML = sum.value;
+                console.log(sum.value)*/ 
